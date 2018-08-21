f93eb7bc64a770751ce3b4beeb132824e33a99ea
DianaBravo <42352948+DianaBravo@users.noreply.github.com>
2018-08-20 20:56:45 -0500

    Update cuenta.py
    Diana Marisol Medina Bravo
    A01748753
    Grupo 04

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Diana Marisol Medina Bravo, A01748753
+# Descripcion: Se calcula el total a pagar sumando el porcentaje de propina, el porcentaje del iva y el costo inicial de la comida.
 
 # Escribe tu programa después de esta línea.
+
+costoComida=int(input("Introduzca el costo de la comida:"))
+
+propina=costoComida*0.13
+iva=costoComida*0.15
+totalAPagar= costoComida+propina+iva
+
+print("Propina es:$%5.2f" % propina)
+print("IVA:$%5.2f" % iva)
+print("El total a pagar es:$%5.2f" % totalAPagar)
+
